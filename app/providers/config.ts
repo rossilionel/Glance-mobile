ec85d3b79405e9b04a88f8c11d8b0bf9b6a03462
Lionel <lionel_rossiok@hotmail.com>
2016-12-14 20:20:26 -0800

    glance-mobile

---
+++ app/providers/config.ts
@@ -0,0 +1,15 @@
+//Automatically generated on Sun Dec 11 05:05:11 JST 2016 by future
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class ClientConfig {
+    public baseUrl: string = 'https://serve.glanceon.com';
+    public appName: string = 'Glance (local)';
+    public build = {
+        counter: '',
+        env_name: '',
+        server_counter: '',
+        app_counter: ''
+    };
+}
+
